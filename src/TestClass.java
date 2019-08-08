ba82c4930c0aa2cc198f96809b81b8fd39b4d552
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-08-08 13:42:07 +0200

    Change loops.

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,26 +4,23 @@ public class TestClass {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Thing[] things = new Thing[3];
-        int a=0;
-        while (a<things.length){
-
+        int a = 0;
+        Thing b;
+        while (a < things.length) {
             System.out.println("Podaj unikalne wartości. Podaj imię i wiek: ");
-                things[a] = new Thing(scan.nextLine(), scan.nextInt());
-                if(a!=0) {
-                    if (things[a].equals(things[a - 1])) {
-                        scan.nextLine();
-                        System.out.println("Wartość nie jest unikalna, wprowadź jeszcze raz.");
-                    } else {
-                        scan.nextLine();
-                        a++;
-                    }
-                }else {
-                    scan.nextLine();
-                    a++;
+            b = new Thing(scan.nextLine(), scan.nextInt());
+            for (int i = 0; i < things.length; i++) {
+                if (b.equals(things[i])) {
+                    System.out.println("Wartości nie są unikalne");
                 }
-
-
+            }
+            b = things[a];
+            a++;
+            scan.nextLine();
         }
+
+
         System.out.println("Wartości są odpowiednie i unikalne!");
     }
 }
+
